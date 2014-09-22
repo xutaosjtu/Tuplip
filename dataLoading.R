36b952045714236901d2b1f55eb5659f6151987a
Tao Xu <xutaosjtu@gmail.com>
2014-09-22 09:52:36 +0200

    Separate data loading as one modual

---
+++ dataLoading.R
@@ -0,0 +1,24 @@
+## Read data file into R and do basic preprcessing including,
+setwd("../../../Dropbox/Diabetes/")
+
+## 1. load metabolite measurements
+data = read.csv("Data/merge_data.csv")
+indx.ref= grep("Ref", data$Sample.Identification)
+
+## 2. remove reference samples
+data.samples = data[-indx.ref, ]
+metabolites = colnames(data)[(10:239)*2-1]
+
+## 3. remove below LOD measurements
+setNAs = function(i, dat){
+  mapply(function(x, y){if(y=="<LOD"|is.na(y)) x=NA; return(x)},dat[,i*2-1], dat[,i*2])
+}
+data.samples[,metabolites] = sapply(10:239, setNAs, dat = data.samples)
+
+## 4. load phenotype data
+pheno = read.csv("Data/sc.csv")
+data.samples = merge(data.samples, pheno)
+
+## 5. load the list valid metabolites
+valid_measures = read.csv("Valid measures.csv",stringsAsFactors=F)
+valid_measures = valid_measures[which(valid_measures[,2]),1]
--- metabolite_summary.R
+++ metabolite_summary.R
@@ -1,21 +1,4 @@
-setwd("../../../Dropbox/Diabetes/")
-
-data = read.csv("Data/merge_data.csv")
-indx.ref= grep("Ref", data$Sample.Identification)
-
-data.samples = data[-indx.ref, ]
-metabolites = colnames(data)[(10:239)*2-1]
-
-setNAs = function(i, dat){
-  mapply(function(x, y){if(y=="<LOD"|is.na(y)) x=NA; return(x)},dat[,i*2-1], dat[,i*2])
-}
-data.samples[,metabolites] = sapply(10:239, setNAs, dat = data.samples)
-
-pheno = read.csv("Data/sc.csv")
-data.samples = merge(data.samples, pheno)
-
-valid_measures = read.csv("Valid measures.csv",stringsAsFactors=F)
-valid_measures = valid_measures[which(valid_measures[,2]),1]
+source("dataLoading.R")
 
 ##Calculate the mean and sd of the metabolites
 metabo.mean = sapply(data.samples[, metabolites], 

3c8e56d941a3933ff309a3bc42d8f658b7f13972
Tao Xu <xutaosjtu@gmail.com>
2014-09-22 09:46:13 +0200

    + analysis for the association with insulin sensitivity

--- association.R
+++ association.R
@@ -55,6 +55,15 @@ for(m in metabolites){
                 ,id = ID, data = data.samples, 
                 na.action=na.omit, family = binomial, contrasts = "exchangeable")
     
+    model = gee(m ~ time.point*Group #+ as.factor(SEX) + AGE 
+                #+ BMI + Syst.0 + HDLMG + HBA1C 
+                #+ BZN0 + INS0_neu
+                ,id = ID, data = data.samples, 
+                na.action=na.omit,  contrasts = "exchangeable")
+    model.coef = coef(summary(model))
+    pvalues = 2 * pnorm(abs(model.coef)[,5], lower.tail = FALSE)
+    model.coef = cbind(model.coef, pvalues)
+    
     model.coef = coef(summary(model))[2,]
     pvalues = 2 * pnorm(abs(model.coef)[5], lower.tail = FALSE)
     rst = rbind(rst, c(model.coef, pvalue = pvalues))
@@ -64,4 +73,97 @@ for(m in metabolites){
 rownames(rst) = metabolites
 
 write.csv(rst, file = "unadjusted model.csv")
+
+## Apply mixed effect model
+require("lme4")
+require("lmerTest")
+require(nlme)
+model = lmer(Asp ~  time.point*Group + (time.point|ID) , 
+   data = data.samples)
+# 
+# model = lme(Asp ~ 1, 
+#              data = data.samples,
+#              random = ~time.point-1|Group)
+
+data.samples$time.point = as.factor(data.samples$time.point)
+data.samples$Group = factor(data.samples$Group, levels = c("NGT", "IGT"))
+
+rst = NULL
+for(m in metabolites){
+  data.samples$m = data.samples[,m]
+  if(m %in% valid_measures){
+    
+    model = lme(m ~ time.point*Group 
+#                 + as.factor(SEX) + AGE 
+#                 + BMI + Syst.0 + HDLMG + HBA1C 
+#                 + BZN0 + INS0_neu 
+                , na.action = na.omit,
+                random = ~1|ID, 
+                data = data.samples)
+    
+    tmp = summary(model)$tTable
+    model.coef = c(tmp[2,], tmp[3,], tmp[nrow(tmp)-1,],tmp[nrow(tmp),])
+    rst = rbind(rst, model.coef)
+  }
+  else rst = rbind(rst, NA)
+}
+rownames(rst) = metabolites
+
+write.csv(rst, file = "full model_lme_categorized.csv")
+
+
+## Association with insulin sensitivity
+rst = NULL
+for(m in valid_measures){
+
+  metabo.3t = matrix(data.samples[,m],ncol = 3, byrow = T)
+  colnames(metabo.3t) = c("T0", "T60", "T120")
+  tmppheno = data.samples[which(data.samples$time.point==1),colnames(pheno)]
+  tmpdata = data.frame(tmppheno, metabo.3t)
+  
+  model = lm(log(ISIMATS_neu) ~ T0 + T60 + T120 
+              + as.factor(SEX) + AGE 
+              + BMI + Syst.0 + HDLMG 
+#              + HBA1C + BZN0 + INS0_neu 
+             , na.action = na.omit, 
+             data = tmpdata)
+  rst = rbind(rst, summary(model)$coef[2,])
+}
+rownames(rst) = valid_measures
+write.csv(rst, file = "associations between T120 metabolite leve and insulin sensitivity_multivaraite model_2.csv")
+
+
+model = lm(ISIMATS_neu ~ BZN0 + INS0_neu 
+           + BZ60 + INS60_neu
+           + BZ120 + INS120_neu
+           , na.action = na.omit, 
+           data = tmpdata)
+plot(tmpdata$ISIMATS_neu, exp(predict(model1)))
+plot(tmpdata$ISIMATS_neu, predict(model2))
+
+pairs(tmpdata[, c("T0", "T60", "T120", "ISIMATS_neu")])
+
+# plot of correlations
+cor1 = cor(x = data.samples$ISIMATS_neu[which(data.samples$time.point==1)], 
+    y = data.samples[which(data.samples$time.point==1),valid_measures[-c(156,157)]],
+    use = "pair", method = "spearman")
+cor2 = cor(x = data.samples$ISIMATS_neu[which(data.samples$time.point==2)], 
+           y = data.samples[which(data.samples$time.point==2),valid_measures[-c(156,157)]],
+           use = "pair", method = "spearman")
+cor3 = cor(x = data.samples$ISIMATS_neu[which(data.samples$time.point==3)], 
+           y = data.samples[which(data.samples$time.point==3),valid_measures[-c(156,157)]],
+           use = "pair", method = "spearman")
+cor.ISIMATS_metabo = t(rbind(cor1, cor2, cor3))
+
+require(corrplot)
+pdf("correlation between Insulin sensitivity and metabolties.pdf", width = 4, height = 10)
+for( i in 1:8){
+  if(i*20<nrow(cor.ISIMATS_metabo)){
+    corrplot(cor.ISIMATS_metabo[(i-1)*20+1:20,], method = "number")
+  }
+  else corrplot(cor.ISIMATS_metabo[((i-1)*20+1):nrow(cor.ISIMATS_metabo),], method = "number")
+}
+dev.off()
+
+
 
--- metabolite_summary.R
+++ metabolite_summary.R
@@ -14,6 +14,9 @@ data.samples[,metabolites] = sapply(10:239, setNAs, dat = data.samples)
 pheno = read.csv("Data/sc.csv")
 data.samples = merge(data.samples, pheno)
 
+valid_measures = read.csv("Valid measures.csv",stringsAsFactors=F)
+valid_measures = valid_measures[which(valid_measures[,2]),1]
+
 ##Calculate the mean and sd of the metabolites
 metabo.mean = sapply(data.samples[, metabolites], 
                      function(x){
@@ -23,8 +26,38 @@ metabo.sd = sapply(data.samples[, metabolites],
                    function(x){
                      tapply(x, INDEX = interaction(data.samples$time.point,data.samples$Group), sd, na.rm = T)
                    })
-
 write.csv(rbind(metabo.mean, metabo.sd), file = "metabolite mean and sd.csv")
+
+
+## Average value and sd of blood sugar("BZN0","BZ60","BZ120") and insulin ("INS0_neu" ,"INS60_neu","INS120_neu")
+Mean = sapply(pheno[,c("INS0_neu" ,"INS60_neu","INS120_neu")], 
+              function(x){
+                tapply(x, INDEX = pheno$Group, mean, na.rm = T)
+                })
+Mean = c(Mean[1,],Mean[2,]) 
+SD = sapply(pheno[,c("INS0_neu" ,"INS60_neu","INS120_neu")], 
+            function(x){
+              tapply(x, INDEX = pheno$Group, sd, na.rm = T)
+            })
+SD = c(SD[1,],SD[2,])
+tmp = data.frame(mean = Mean, 
+                 sd = SD, 
+                 time = rep(c(1:3),2), 
+                 group = rep(c("IGT", "NGT"), each = 3)
+)
+
+pdf("Blood sugar.pdf", width = 10)
+grid.newpage()
+grid.rect(gp=gpar(fill="grey"))
+pushViewport(viewport(layout=grid.layout(2, 2)))
+p = ggplot(tmp, aes(x = time, y = mean, colour = group, group = group)) + 
+  geom_errorbar(aes(ymin = mean-sd/sqrt(73), ymax = mean + sd/sqrt(73)), 
+                width = .1, position = pd) + 
+  geom_line(position = pd) + geom_point(position = pd) +
+  ggtitle("Blood sugar")
+print(p)
+dev.off()
+
 
 
 ## plot
@@ -36,9 +69,56 @@ vplayout <- function(x, y) viewport(layout.pos.row = x, layout.pos.col = y)
 
 pdf("test.pdf", width = 10)
 ## position matrix of the plots
-position = rbind(c(1,1), c(1,2), c(2,1), c(2,2))
-for(i in 1:ncol(metabo.mean)){
+position = rbind(c(1,1), c(1,2), c(2,1), c(2,2)); k = 1
+for(i in which(colnames(metabo.mean) %in% valid_measures)){
   
+  ## for every four figures great a new page
+  if(k %% 4 == 1){
+    grid.newpage()
+    grid.rect(gp=gpar(fill="grey"))
+    pushViewport(viewport(layout=grid.layout(2, 2)))
+  }
+  
+    tmp = data.frame(mean = metabo.mean[,i], 
+                     sd = metabo.sd[,i], 
+                     time = rep(c(1:3),2), 
+                     group = rep(c("IGT", "NGT"), each = 3)
+    )
+    
+#     ##Set NA values to zero to make the plot run
+#     tmp[which(is.na(tmp), arr.ind = T)]=0
+    
+    pd <- position_dodge(.1) 
+    p = ggplot(tmp, aes(x = time, y = mean, colour = group, group = group)) + 
+      geom_errorbar(aes(ymin = mean-sd/sqrt(73), ymax = mean + sd/sqrt(73)), 
+                    width = .1, position = pd) + 
+      geom_line(position = pd) + geom_point(position = pd) +
+      ggtitle(colnames(metabo.mean)[i])
+  
+  
+  ## Push the plot to the right grid according to the position matrix
+  if(k %% 4 == 0){
+    print(p, vp = vplayout(x = position[4,1], y = position[4,2]))
+  }
+  else print(p, vp = vplayout(x = position[k %% 4,1], y = position[k %% 4,2])) 
+
+  k = k+1
+}
+dev.off()
+
+# require("gplots")
+# plotmeans(m~ time.point, data = data.samples, subset = data.samples$Group=="IGT", 
+#           col = "red", lab = m)
+# plotmeans(m~ time.point, data = data.samples, subset = data.samples$Group=="NGT", 
+#           add = T, lty = 2, col = "darkgreen")
+
+## Boxplot
+pdf("boxplot.pdf", width = 10)
+## position matrix of the plots
+position = rbind(c(1,1), c(1,2), c(2,1), c(2,2))
+for(i in 1:length(metabolites)){
+  m = metabolites[i]
+   
   ## for every four figures great a new page
   if(i %% 4 == 1){
     grid.newpage()
@@ -46,22 +126,8 @@ for(i in 1:ncol(metabo.mean)){
     pushViewport(viewport(layout=grid.layout(2, 2)))
   }
   
-  
-  tmp = data.frame(mean = metabo.mean[,i], 
-                   sd = metabo.sd[,i], 
-                   time = rep(c(1:3),2), 
-                   group = rep(c("IGT", "NGT"), each = 3)
-                   )
-  
-  ##Set NA values to zero to make the plot run
-  tmp[which(is.na(tmp), arr.ind = T)]=0
-  
   pd <- position_dodge(.1) 
-  p = ggplot(tmp, aes(x = time, y = mean, colour = group, group = group)) + 
-    geom_errorbar(aes(ymin = mean-sd/sqrt(73), ymax = mean + sd/sqrt(73)), 
-                  width = .1, position = pd) + 
-    geom_line(position = pd) + geom_point(position = pd) +
-    ggtitle(colnames(metabo.mean)[i])
+  p = ggplot(data.samples, aes(x = factor(time.point), y = data.samples[,m])) + geom_boxplot(aes(fill=Group)) + ggtitle(m) + xlab("Concentration (uM)") + ylab("Time Point")
   
   ## Push the plot to the right grid according to the position matrix
   if(i %% 4 == 0){
@@ -71,9 +137,24 @@ for(i in 1:ncol(metabo.mean)){
 }
 dev.off()
 
-# require("gplots")
-# plotmeans(m~ time.point, data = data.samples, subset = data.samples$Group=="IGT", 
-#           col = "red", lab = m)
-# plotmeans(m~ time.point, data = data.samples, subset = data.samples$Group=="NGT", 
-#           add = T, lty = 2, col = "darkgreen")
+
+
+
+pdf("Insulin sensitivity index associated with metabolites.pdf", width = 10, height = 4)
+mf_labeller <- function(var, value){
+  value <- as.character(value)
+  if (var=="time.point") { 
+    value[value=="1"] <- "T0"
+    value[value=="2"]   <- "T60"
+    value[value=="3"]   <- "T120"
+  }
+  return(value)
+}
+p = ggplot(data.samples, aes(log(C2), log(ISIMATS_neu))) + geom_point() + facet_grid(.~time.point, labeller = mf_labeller) + stat_smooth(method = "lm") + ggtitle("C2") +xlab("Log metabolite concentration") + ylab("Log insulin sensitivity score")
+print(p)
+p = ggplot(data.samples, aes(log(Gly), log(ISIMATS_neu))) + geom_point() + facet_grid(.~time.point, labeller = mf_labeller) + stat_smooth(method = "lm") + ggtitle("Glycine") +xlab("Log metabolite concentration") + ylab("Log insulin sensitivity score")
+print(p)
+p = ggplot(data.samples, aes(log(lysoPC.a.C18.2), log(ISIMATS_neu))) + geom_point() + facet_grid(.~time.point, labeller = mf_labeller) + stat_smooth(method = "lm") + ggtitle("LPC (18:2)") +xlab("Log metabolite concentration") + ylab("Log insulin sensitivity score")
+print(p)
+dev.off()
 
